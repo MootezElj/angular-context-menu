fea9b743a79a721315213997a464aea91607b32f
MootezElj <eljmootez@gmail.com>
2021-08-21 13:02:07 +0100

    updates context menu style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,12 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {SimpleToolbarComponent} from './components/toolbar/simple-toolbar/simple-toolbar.component';
-import { SimpleContextMenuComponent } from './components/context-menus/simple-context-menu/simple-context-menu.component';
-import { ParentComponent } from './components/parent/parent.component';
+import {SimpleContextMenuComponent} from './components/context-menus/simple-context-menu/simple-context-menu.component';
+import {ParentComponent} from './components/parent/parent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SimpleToolbarComponent,
     SimpleContextMenuComponent,
     ParentComponent
   ],
